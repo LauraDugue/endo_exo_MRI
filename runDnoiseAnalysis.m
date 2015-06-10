4cb390cde46e080448eaeca35965aff637780dc4
LauraDugue <purpadmin@hypatia.cns.nyu.edu>
2015-06-10 10:48:21 -0400

    run co extra exo group

--- runDnoiseAnalysis.m
+++ runDnoiseAnalysis.m
@@ -5,19 +5,19 @@
 %       date: 01/17/15
 
 %% set conditions to run
-obs = 'nms';
-cond = 'endo'; % 'exo' or 'endo'
-createGroup = 0; % 0 to not create and 1 to create
+obs = 'co';
+cond = 'exo'; % 'exo' or 'endo'
+createGroup = 1; % 0 to not create and 1 to create
 whichAnal = 'TPJ'; % 'first' or 'visualCortex' or 'TPJ'
-doGLM = 1; % 0 to not run and 1 to run
+doGLM = 0; % 0 to not run and 1 to run
 roiName = {'r_vTPJ', 'r_Ins'};%, 'r_pTPJ'
     %{'r_v1', 'r_v3a','r_v4','r_v7','r_vTPJ'};
     %{'l_v1', 'l_v4', 'l_vo1', 'l_vo2', 'l_v2d', 'l_v3d', 'l_v3a', 'l_v3b', 'l_lo1', 'l_lo2','l_v7','l_ips1','l_ips2','l_ips3','l_ips4',...
     %'r_v1', 'r_v4', 'r_vo1', 'r_vo2', 'r_v2d', 'r_v3d', 'r_v3a', 'r_v3b', 'r_lo1', 'r_lo2','r_v7','r_ips1','r_ips2','r_ips3','r_ips4',...
     %'r_vTPJ'};
-plotData = 1; % 0 to not plot the results and 1 to plot the results
+plotData = 0; % 0 to not plot the results and 1 to plot the results
 
-%% set parameters fro mrTool
+%% set parameters for mrTool
 % open a new view
 v = newView;
 % get attention condition
