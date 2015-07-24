5a194dd046a8b3371a2c0c3211bbeeed5e906762
LauraDugue <purpadmin@hypatia.cns.nyu.edu>
2015-07-24 13:00:33 -0400

    fixed effect option 2

--- statsBootFixedeffectoption2.m
+++ statsBootFixedeffectoption2.m
@@ -1,10 +1,11 @@
-% statsBootFixedeffect.m
+% statsBootFixedeffectoption2.m
 %
-%      usage: statsBootFixedeffect
+%      usage: statsBootFixedeffectoption2
 %         by: eli & laura
 %       date: 07/02/15
 
-%%% Compute a distribution of randomised differences averaged across observers
+%%% This program run a GLM with a separate column for each trial to compute the response amplitudes separately for each trial.
+%%% Then compute a distribution of randomised differences averaged across observers
 
 %% STEP 1: Compute the actual betas for all 5 observers and save it
 
@@ -12,8 +13,8 @@
 obs = {'nms' 'mr' 'id' 'rd' 'co'}; %
 whichAnal = 'first'; % 'first' or 'TPJ'
 roiName = {'r_vTPJ','r_pTPJ','r_Ins'};%
-attCond = 'endo';
-saveOverlay = 1;
+attCond = 'exo';
+saveOverlay = 0;
 
 for iObs = 1:length(obs)
     
@@ -29,16 +30,6 @@ for iObs = 1:length(obs)
     
     %% load the output of the GLMdenoise
     load(['glmoutput_' attCond '_' whichAnal '_CI_' obs{iObs} '_results.mat'])
-    
-    %% make the unshuffled design matrix
-    scm = [];
-    
-    for iRun = 1:length(results.inputs.design)
-        % make the design matrix
-        thisDesign = convn(results.models{1}(:,1), results.inputs.design{iRun});
-        thisDesign = thisDesign(1:length(results.inputs.design{iRun}),:);
-        scm = cat(1, scm, thisDesign);
-    end
     
     %% save Bootstraped data as a mrTool overlay
     if saveOverlay
@@ -91,6 +82,31 @@ for iObs = 1:length(obs)
         tSeries{iRoi} = tempB;
     end
     
+    %% make the unshuffled design matrix: 1 column per trial
+    idxAll = [];
+    for iRun = 1:length(results.inputs.design)
+        idx = [];
+        for iVol = 1:length(results.inputs.design{iRun})
+            if find(results.inputs.design{iRun}(iVol,:)==1) > 0
+                idx(iVol) = 1;
+            elseif isempty(find(results.inputs.design{iRun}(iVol,:)==1))
+                idx(iVol) = 0;
+            end
+        end
+        idxAll = [idxAll idx];
+    end
+    
+    scm = zeros(size(idxAll,2),size(find(idxAll==1),2));
+    countTrial = 1;
+    for iVol = 1:size(idxAll,2)
+        if idxAll(iVol) == 1
+            scm(iVol,countTrial) = 1;
+            countTrial = countTrial + 1;
+        end
+    end
+    thisDesign = convn(results.models{1}(:,1), scm);
+    scm = thisDesign(1:size(scm,1),:);
+    
     %% Compute the actual contrast (compute the betas using standard GLM)
     for iRoi = 1:length(localizer)
         betas{iRoi,iObs} = regress(tSeries{iRoi}', scm);
@@ -98,7 +114,7 @@ for iObs = 1:length(obs)
     
     mrQuit
 end
-save(['/Volumes/DRIVE1/DATA/laura/MRI/Group/betas_' attCond '.mat'],'betas')
+save(['/Volumes/DRIVE1/DATA/laura/MRI/Group/betas_' attCond '_indTrials.mat'],'betas')
 
 %% STEP 2: Compute the randomized betas for all 5 observers and save it
 
@@ -149,21 +165,7 @@ end
 rep = 100000;
 for iRep = 1:rep
     disp(['Running repetition number: ' num2str(iRep)])
-    for iObs = 1:length(obs)
-        % make the shuffled design matrix
-        scm = [];
-        for iRun = 1:length(dataGLM{iObs}.inputs.design)
-            % make the unshuffled design matrix
-            thisDesign = convn(dataGLM{iObs}.models{1}(:,1), dataGLM{iObs}.inputs.design{iRun});
-            thisDesign = thisDesign(1:length(dataGLM{iObs}.inputs.design{iRun}),:);
-            scm = cat(1, scm, thisDesign);
-        end
-        
-        % shuffle the design matrix
-        idx = size(scm,2);
-        idxShuffled = randsample(1:idx,idx);
-        scmShuffled = scm(:,idxShuffled);
-        
+    parfor iObs = 1:length(obs)
         % pull data out of ROI and select voxels based on stimulus localizer
         for iRoi = 1:length(localizer{iObs})
             goodVox{iObs}{iRoi} = localizer{iObs}{iRoi}.co > locThresh & localizer{iObs}{iRoi}.ph < pi & ~isnan(rois{iObs}{iRoi}.ehdr(localizer{iObs}{iRoi}.goodSelectedVoxel)');
@@ -180,77 +182,44 @@ for iRep = 1:rep
             tSeries{iObs,iRoi} = tempB;
         end
         
+        % make the shuffled design matrix: 1 column per trial
+        idxAll = [];
+        for iRun = 1:length(dataGLM{iObs}.inputs.design)
+            idx = [];
+            for iVol = 1:length(dataGLM{iObs}.inputs.design{iRun})
+                if find(dataGLM{iObs}.inputs.design{iRun}(iVol,:)==1) > 0
+                    idx(iVol) = 1;
+                elseif isempty(find(dataGLM{iObs}.inputs.design{iRun}(iVol,:)==1))
+                    idx(iVol) = 0;
+                end
+            end
+            idxAll = [idxAll idx];
+        end
+        
+        scm = zeros(size(idxAll,2),size(find(idxAll==1),2));
+        countTrial = 1;
+        for iVol = 1:size(idxAll,2)
+            if idxAll(iVol) == 1
+                scm(iVol,countTrial) = 1;
+                countTrial = countTrial + 1;
+            end
+        end
+        thisDesign = convn(dataGLM{iObs}.models{1}(:,1), scm);
+        scm = thisDesign(1:size(scm,1),:);
+        
+        % shuffle the design matrix
+        idx = size(scm,2);
+        idxShuffled = randsample(1:idx,idx);
+        scmShuffled = scm(:,idxShuffled);
+        
         % Compute the surrogate contrasts
         for iRoi = 1:length(roiName)
-            betasShuffled{iRoi,iObs} = regress(tSeries{iObs,iRoi}', scmShuffled);
+            betasShuffled{iRoi,iObs}(iRep,:) = regress(tSeries{iObs,iRoi}', scmShuffled);
         end
     end
-    for iRoi = 1:length(roiName)
-        betasShuf{iRoi} = [betasShuffled{iRoi,1},betasShuffled{iRoi,2},betasShuffled{iRoi,3},betasShuffled{iRoi,4},betasShuffled{iRoi,5}];
-        betasShuf{iRoi} = mean(betasShuf{iRoi},2);
-        randombetas{iRoi}(iRep,:) = betasShuf{iRoi}';
-    end
 end
 
-save(['/Volumes/DRIVE1/DATA/laura/MRI/Group/randombetas_' attCond '.mat'],'randombetas')
-
-%% ask whether the actual contrast is larger than 95th percentile of shuffled distribution
-% set conditions to run
-obs = {'nms' 'mr' 'id' 'rd' 'co'}; %
-whichAnal = 'first'; % 'first' or 'TPJ'
-roiName = {'r_vTPJ','r_pTPJ','r_Ins'};%
-attCond = 'endo';
-rep = 100000;
-
-% Load the data
-cd /Volumes/DRIVE1/DATA/laura/MRI/Group
-load(['randombetas_' attCond '.mat'])
-load(['betas_' attCond '.mat'])
-
-% Average the betas accross observers
-for iRoi = 1:length(roiName)
-    temp{iRoi} = [betas{iRoi,1},betas{iRoi,2},betas{iRoi,3},betas{iRoi,4},betas{iRoi,5}];
-    actualbetas{iRoi} = mean(temp{iRoi},2);
-    actualbetas{iRoi} = actualbetas{iRoi}';
-end
-
-if strcmp(whichAnal,'TPJ')
-    contrast = [0 0 0 -1 1 -1 1 0]';
-    contrastCorrect = [0 0 0 -1 1 0 0 0]';
-    contrastIncorrect = [0 0 0 0 0 -1 1 0]';
-elseif strcmp(whichAnal,'first')
-    contrast = [-1 1 -1 1 -1 1 -1 1 0 0 0 0]'; % PRE AND POST
-    contrastPre = [-1 1 0 0 -1 1 0 0 0 0 0 0]'; % PRE
-    contrastPost = [0 0 -1 1 0 0 -1 1 0 0 0 0]'; % POST
-end
-for iRoi = 1:3
-    for iBoot=1:rep
-        if strcmp(whichAnal,'TPJ')
-            my_contrast_distribution_cont{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrast;
-            my_contrast_distribution_contCor{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrastCorrect;
-            my_contrast_distribution_contIncor{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrastIncorrect;
-        elseif strcmp(whichAnal,'first')
-            my_contrast_distribution_cont{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrast;
-            my_contrast_distribution_contPre{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrastPre;
-            my_contrast_distribution_contPost{iRoi}(iBoot) = randombetas{iRoi}(iBoot,:) * contrastPost;
-        end
-    end
-    if strcmp(whichAnal,'TPJ')
-        actualContrast{iRoi} = actualbetas{iRoi} * contrast;
-        actualContrastCor{iRoi} = actualbetas{iRoi} * contrastCorrect;
-        actualContrastIncor{iRoi} = actualbetas{iRoi} * contrastIncorrect;
-        statsPerRoi{iRoi,1} = prctile(my_contrast_distribution_cont{iRoi}, 97.5) < actualContrast{iRoi};
-        statsPerRoi{iRoi,2} = prctile(my_contrast_distribution_contCor{iRoi}, 97.5) < actualContrastCor{iRoi};
-        statsPerRoi{iRoi,3} = prctile(my_contrast_distribution_contIncor{iRoi}, 97.5) < actualContrastIncor{iRoi};
-    elseif strcmp(whichAnal,'first')
-        actualContrast{iRoi} = actualbetas{iRoi} * contrast;
-        actualContrastPre{iRoi} = actualbetas{iRoi} * contrastPre;
-        actualContrastPost{iRoi} = actualbetas{iRoi} * contrastPost;
-        statsPerRoi{iRoi,1} = prctile(my_contrast_distribution_cont{iRoi}, 98) < actualContrast{iRoi};
-        statsPerRoi{iRoi,2} = prctile(my_contrast_distribution_contPre{iRoi}, 98) < actualContrastPre{iRoi};
-        statsPerRoi{iRoi,3} = prctile(my_contrast_distribution_contPost{iRoi}, 98) < actualContrastPost{iRoi};
-    end
-end
+save(['/Volumes/DRIVE1/DATA/laura/MRI/Group/randombetas_' attCond '_indTrials.mat'],'betasShuffled')
 
 
 
